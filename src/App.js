4c28fe875b248f7666e71448bd0c782e7c1f925a
agathyweb <agathyweb@gmail.com>
2023-03-12 22:50:57 +0800

    appjs change name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Little Lemon
+          Little Lemon Restaurant
         </a>
       </header>
     </div>
