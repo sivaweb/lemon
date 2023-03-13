b906e565c6f09c84a47a3b2df1e79af5810fd0e0
agathyweb <agathyweb@gmail.com>
2023-03-14 00:27:12 +0800

    p1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 function App() {
   return (
     <div className="App">
-     <h1>Little Lemon restaurant</h1>
+     <h1>Little Lemon restaurant</h1>\
+     <div><p>i am working on little lemon project</p></div>
     </div>
   );
 }
